d89b9dff7175ad024184cb9da497ac3acc07c949
Pavlo-Hnatenko <gnatenko.pavlo@gmail.com>
2020-08-02 21:02:28 +0300

    first commit: classes DateFormatter and UniqueNameFinder were added

---
+++ src/main/java/task1/DateFormatter.java
@@ -0,0 +1,19 @@
+package task1;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DateFormatter {
+
+    public static List<String > formatDate(List<LocalDate> originDates) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        return originDates.stream()
+                .filter(localDate -> localDate.toString().matches("\\d{2,4}[/-]\\d{2,4}[/-]\\d{2,4}"))
+                .map(localDate -> localDate.format(formatter))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/task2/UniqueNameFinder.java
@@ -0,0 +1,28 @@
+package task2;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class UniqueNameFinder {
+
+    protected static String findUniqueName(ArrayList<String> names) {
+
+        Integer countdown = 0; // to avoid repeated autoboxing below
+        final LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+
+        for (String s : names) {
+            Integer count = map.getOrDefault(s, countdown);
+            map.put(s, count + 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return "there aren't unique elements";
+
+    }
+}
